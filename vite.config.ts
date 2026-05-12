5b82b234c4fa407dba40e0152b4190894afc4e60
Hazaadi <104632617+Hazaadi@users.noreply.github.com>
2026-05-12 08:55:06 +0100

    Fix routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+   publicDir: 'public',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
